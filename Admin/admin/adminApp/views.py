46a1aeb6b9197f0e5ff49a779994aa1ca39e367a
Anjana P R <pranjana333@gmail.com>
2026-01-08 15:53:25 +0530

    Initialize Django project structure with basic settings, URLs, views, and templates for the OTT Streaming Platform admin interface.

---
+++ Admin/admin/adminApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def login_page(request):
+    return render(request, './login.html')
+def home_page(request):
+    return render(request, './home.html')
+def movielist_page(request):
+    return render(request, './movielist.html')
+def userlist_page(request):
+    return render(request, './userlist.html')
+def report_page(request):
+    return render(request, './report.html')
+def movieview_page(request):
+    return render(request, './movieview.html')
