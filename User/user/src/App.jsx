d9dacf07388e599e5f8d60a64943bd071f723288
Anjana P R <pranjana333@gmail.com>
2026-01-11 23:12:59 +0530

    Refactor CSS styles: update global styles, enhance button and input designs, and improve layout for responsiveness

--- User/user/src/App.jsx
+++ User/user/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Router from "./router";
 
+
+
 export default function App() {
     return (
         <BrowserRouter>
