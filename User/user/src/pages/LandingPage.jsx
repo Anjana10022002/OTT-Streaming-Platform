9ede52fb8bb3b618379ed465b6f525c76a2bc5a9
Anjana P R <pranjana333@gmail.com>
2026-01-13 20:06:17 +0530

    Editing and styling landing page

--- User/user/src/pages/LandingPage.jsx
+++ User/user/src/pages/LandingPage.jsx
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom";
 
 function Landing() {
     return (
-        <div className="landing-page">
-            <div className="landing-center">
-                {/* <img src="https://images.unsplash.com/photo-1524985069026-dd778a71c7b4" alt="OTT Preview" /> */}
+        <div className="hero">
+            <img
+                src="https://images.unsplash.com/photo-1524985069026-dd778a71c7b4"
+                alt="OTT Background"
+                className="hero-bg"
+            />
 
+            <div className="hero-overlay"></div>
+
+            <div className="hero-content">
                 <h1>Unlimited Movies, Shows & Entertainment</h1>
                 <p>Watch anywhere. Cancel anytime.</p>
 
-                <div className="landing-actions">
+                <div className="hero-actions">
                     <Link to="/register" className="btn primary">Sign Up</Link>
                     <Link to="/login" className="btn secondary">Login</Link>
                 </div>
