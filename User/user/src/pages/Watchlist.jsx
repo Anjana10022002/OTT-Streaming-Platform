77e43a3225199c864d1fd32a7dfd1ee48d7e3b9f
Anjana P R <pranjana333@gmail.com>
2026-01-13 15:54:16 +0530

    Remove extra MovieCard component from Watchlist page

--- User/user/src/pages/Watchlist.jsx
+++ User/user/src/pages/Watchlist.jsx
@@ -17,7 +17,6 @@ function Watchlist() {
                     <MovieCard />
                     <MovieCard />
                     <MovieCard />
-
                 </div>
             </div>
         </>
