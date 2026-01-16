25e4b1603e70412b690c6a1dd1fc1512928a2526
Anjana P R <pranjana333@gmail.com>
2026-01-16 15:34:52 +0530

    Fix user creation methods in UserManager for proper superuser assignment

--- Admin/admin/adminApp/models.py
+++ Admin/admin/adminApp/models.py
@@ -1,10 +1,8 @@
-Mashupstack:
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager 
 from django.db import models 
 
-
 class UserManager(BaseUserManager): 
-    def create_user(self, email, password=None): 
+    def create_user(self, email, password=None): #User
           if not email: 
             raise ValueError("Users must have an email address") 
             email = self.normalize_email(email) 
@@ -12,10 +10,10 @@ class UserManager(BaseUserManager):
             user.set_password(password) 
             user.save(using=self._db) 
             return user 
-          def create_superuser(self, email, password): 
+          def create_superuser(self, email, password): #Admin
             user = self.create_user(email, password) 
             user.is_admin = True 
-            user.is_superuser = True 
+            User.is_superuser = True 
             user.save(using=self._db) 
             return user 
  
