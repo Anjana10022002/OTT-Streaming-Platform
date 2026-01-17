2f622c797bcbc4cc74ff369cc1c9404a3fbaa967
Anjana P R <pranjana333@gmail.com>
2026-01-17 20:59:54 +0530

    Craeting user signup api

--- Admin/admin/userapi/views.py
+++ Admin/admin/userapi/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from django.contrib.auth.forms import UserCreationForm
+from rest_framework import permissions
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def signup(request):
+    form = UserCreationForm(data=request.data)
+    if form.is_valid():
+        user = form.save()
+        return Response("account created successfully", status=status.HTTP_201_CREATED)
+    return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
